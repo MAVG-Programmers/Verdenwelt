0f3b2cf0eabc96ffe3983b9a69bf354dd8087661
Mindar <alexander.schramm33@gmail.com>
2014-12-21 02:04:21 +0100

    Added Gameclient

---
+++ Verdenwelt.Client/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Verdenwelt.Client
+{
+    public abstract class Entity
+    {
+        public List<IComponent> Components { get; set; }
+
+        public void AddComponent(IComponent component) 
+        {
+            
+            //Add component to component list and do all the inter-component linking
+             
+        }
+    }
+}
---
+++ Verdenwelt.Client/IComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Verdenwelt.Client
+{
+    public interface IComponent
+    {
+    }
+}
