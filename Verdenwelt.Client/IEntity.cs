b125d7994b21c3eecfc1971897718184a41c9386
Njaal Tangye <belbert95@me.com>
2014-12-24 11:01:24 +0100

    Added Interfaces

---
+++ Verdenwelt.Client/IEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Verdenwelt.Client
+{
+    interface IEntity
+    {
+        Vector2 Position;
+        void Load();
+        void Udate(VerdenweltGame g, float deltaTime);
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
